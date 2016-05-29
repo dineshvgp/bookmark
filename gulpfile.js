6bf70fa835d5ede361c5f298d29b87995119a0e3
dineshi2it <dinesh.vgp@gmail.com>
2016-05-30 00:40:56 +0530

    Removed main bower file and bower component path added

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,6 @@
 let source = require("vinyl-source-stream");
 let gulp = require("gulp");
 let browserify = require("browserify");
-var mainBowerFiles = require("main-bower-files");
 var concat = require("gulp-concat");
 var uglify = require("gulp-uglify");
 var minifyCSS = require("gulp-minify-css");
@@ -14,8 +13,7 @@ var minifyCSS = require("gulp-minify-css");
 //http://stackoverflow.com/questions/33801311/webpack-babel-6-es6-decorators
 gulp.task("browserify", function() {
   browserify({
-    entries: "./client/app.js",
-    debug: true
+    entries: "./client/app.js"
   })
   .transform("babelify", {
     "presets": ["react", "es2015"],
@@ -31,20 +29,31 @@ gulp.task("browserify:watch", function() {
   gulp.watch("./client/**/*.js", ["browserify"]);
 });
 
-//Copy bower js and custom js
-gulp.task("copy:js", function() {
-  gulp.src(["assets/js/**/*"])
-    .pipe(concat("main.js"))
-    .pipe(uglify())
-    .pipe(gulp.dest("public/assets"));
+//Copy bower js to assets
+gulp.task("bower:js", function() {
+  gulp.src([
+    "bower_components/jquery/dist/jquery.js",
+    "bower_components/Materialize/bin/materialize.js"
+  ])
+  .pipe(concat("bower.min.js"))
+  .pipe(uglify())
+  .pipe(gulp.dest("public/assets"));
 });
 
-//Copy bower css and custom css
-gulp.task("copy:css", function() {
+//Copy bower css
+gulp.task("bower:css", function() {
+  gulp.src(["bower_components/Materialize/bin/materialize.css"])
+  .pipe(concat("bower.min.css"))
+  .pipe(minifyCSS())
+  .pipe(gulp.dest("public/assets"));
+});
+
+//Copy custom css
+gulp.task("css", function() {
   gulp.src(["assets/css/**/*"])
-    .pipe(concat("style.css"))
-    .pipe(minifyCSS())
-    .pipe(gulp.dest("public/assets"));
+  .pipe(concat("style.min.css"))
+  .pipe(minifyCSS())
+  .pipe(gulp.dest("public/assets"));
 });
 
 //Copy fonts
@@ -57,7 +66,8 @@ gulp.task("copy:fonts", function() {
 
 gulp.task("default", [
   "browserify",
-  "copy:js",
-  "copy:css",
+  "css",
+  "bower:js",
+  "bower:css",
   "copy:fonts"
 ]);
